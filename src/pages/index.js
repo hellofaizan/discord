199b7a1dd637ddf32393075372da5ca3cb17098a
Mohammad Faizan <84437051+hellofaizan@users.noreply.github.com>
2023-02-10 16:19:18 +0530

    Realtime update

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,11 @@ export const getStaticProps = async () => {
   const res = await fetch('https://api.lanyard.rest/v1/users/890232380265222215')
   const data = await res.json()
 
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
   return {
     props: { data },
     revalidate: 5
