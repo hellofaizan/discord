867421f4ee8d344d6d49a1702e7b789455e4ce7d
Mohammad Faizan <84437051+hellofaizan@users.noreply.github.com>
2023-02-11 18:18:36 +0530

    App working crazy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import axios from "axios";
+import { useRef, useState } from "react";
 import { Inter } from '@next/font/google'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import TimeStatus from './timestatus'
+
+import { RiSendPlane2Fill } from "react-icons/ri";
+import { ImSpinner2 } from "react-icons/im";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,27 +15,61 @@ export const getStaticProps = async () => {
   const res = await fetch('https://api.lanyard.rest/v1/users/890232380265222215')
   const data = await res.json()
 
-  // get github followers using fetch
-  const res2 = await fetch('https://api.github.com/users/curiousfaizan')
-  const data2 = await res2.json()
-
   if (!data) {
     return {
       notFound: true,
     }
   }
   return {
-    props: { data, data2 },
+    props: { data },
     revalidate: 15
   }
 }
 
-const Home = ({ data, data2 }) => {
+const Home = ({ data }) => {
   const router = useRouter()
-  console.log(data)
-  console.log(data2)
+
+    const email = useRef("");
+    const message = useRef("");
+    const [sending, setSending] = useState(false);
+    const [errMsg, setErrMsg] = useState("");
+
+    const sendMessage = async () => {
+      if (email.current == "" || message.current == "") return setErrMsg("Please fill out all fields!");
+      const response = new XMLHttpRequest();
+      response.open("POST", "https://discord.com/api/webhooks/1073926474387701792/4KxGXSjYXcu3yZEhhMivFfYwOp_PprAmLSUAyfWYOi-UoD__SvsD5XRPi53ufo_s9xs_");
+      response.setRequestHeader('Content-type', 'application/json');
+      setSending(true);
+
+      const msgEmbed = {
+        title: `Message by `+ email.current,
+        description: message.current,
+        color: hexToDecimal("#ff2bf1"),
+      }
+
+      const params = {
+        username: "dc.hellofaizan.me",
+        content: "<@890232380265222215>",
+        embeds: [msgEmbed]
+      }
+      response.send(JSON.stringify(params));
+
+      setSending(false);
+
+      if (response.status == 200) {
+        setErrMsg("Thanks for reaching out to us!")
+      } else {
+        setErrMsg("Something went wrong, please try again later!")
+      }
+
+      function hexToDecimal(hex) {
+        return parseInt(hex.replace("#",""), 16)
+      }
+      
+  };
+
   if (router.isFallback) {
-    return <div className='flex flex-col items-center justify-center h-screen w-screen py-2'>Loading...</div>
+    return <div className='flex flex-col items-center justify-center h-screen w-screen py-2'>Loading Data...</div>
   }
   const image = "https://cdn.discordapp.com/avatars/" + data.data.discord_user.id + "/" + data.data.discord_user.avatar
 
@@ -39,11 +78,11 @@ const Home = ({ data, data2 }) => {
       {/* Icons Card */}
       <div className='absolute bottom-0 left-0 py-2 md:block hidden ml-10 mb-5'>
         <div className='flex flex-col ml-10 mb-10 space-y-5'>
-          <a href='https://dicord.gg/invite/rraBbMQraQ' target="_blank" rel="noreferrer"><i class="bi bi-discord hover:text-blue-500"></i></a>
-          <a href='https://youtube.com/hellofaizan' target="_blank" rel="noreferrer"><i class="bi bi-youtube hover:text-red-600"></i></a>
-          <a href='https://github.com/hellofaizan' target="_blank" rel="noreferrer"><i class="bi bi-github"></i></a>
-          <a href='https://www.instagram.com/curiousfaizan/' target="_blank" rel="noreferrer"><i class="bi bi-instagram hover:text-[#ff2bf1]"></i></a>
-          <a href='https://twitter.com/HelloFaizandev' target="_blank" rel="noreferrer"><i class="bi bi-twitter hover:text-blue-500"></i></a>
+          <a href='https://dicord.gg/invite/rraBbMQraQ' target="_blank" rel="noreferrer"><i className="bi bi-discord hover:text-blue-500"></i></a>
+          <a href='https://youtube.com/hellofaizan' target="_blank" rel="noreferrer"><i className="bi bi-youtube hover:text-red-600"></i></a>
+          <a href='https://github.com/hellofaizan' target="_blank" rel="noreferrer"><i className="bi bi-github"></i></a>
+          <a href='https://www.instagram.com/curiousfaizan/' target="_blank" rel="noreferrer"><i className="bi bi-instagram hover:text-[#ff2bf1]"></i></a>
+          <a href='https://twitter.com/HelloFaizandev' target="_blank" rel="noreferrer"><i className="bi bi-twitter hover:text-blue-500"></i></a>
         </div>
       </div>
 
@@ -66,13 +105,13 @@ const Home = ({ data, data2 }) => {
                   {data && data.data.activities[0] && data.data.activities[0].state} - <span>
                     {/* If else */}
                     {data && data.data.activities[0] && data.data.discord_status == "dnd" ? (
-                      <span className='text-red-600 font-bold'>{data && data.data.activities[0] && data.data.discord_status} <i class="bi bi-dot font-bold"></i></span>
+                      <span className='text-red-600 font-bold'>{data && data.data.activities[0] && data.data.discord_status} <i className="bi bi-dot font-bold"></i></span>
                     ) : data && data.data.activities[0] && data.data.discord_status == "online" ? (
-                      <span className='text-green-500 font-bold'>{data && data.data.activities[0] && data.data.discord_status} <i class="bi bi-dot font-bold"></i></span>
+                      <span className='text-green-500 font-bold'>{data && data.data.activities[0] && data.data.discord_status} <i className="bi bi-dot font-bold"></i></span>
                     ) : data && data.data.activities[0] && data.data.discord_status == "idle" ? (
-                      <span className='text-yellow-500 font-bold'>{data && data.data.activities[0] && data.data.discord_status} <i class="bi bi-dot font-bold"></i></span>
+                      <span className='text-yellow-500 font-bold'>{data && data.data.activities[0] && data.data.discord_status} <i className="bi bi-dot font-bold"></i></span>
                     ) : (
-                      <span className='text-gray-500'>No Status - Offline <i class="bi bi-dot"></i></span>
+                      <span className='text-gray-500'>No Status - Offline <i className="bi bi-dot"></i></span>
                     )
                     }
                   </span>
@@ -99,34 +138,34 @@ const Home = ({ data, data2 }) => {
             className="absolute inset-x-0 bottom-0 h-1.5 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
           ></span>
 
-          <form action="" className=" space-y-4">
+          <htmlForm action="" className=" space-y-4">
             <p className="text-2xl mt-2 mb-0 font-medium">Let&apos;s chat 💬</p>
             <TimeStatus />
 
             <div>
-              <label for="email" className="text-sm font-medium">Enter Discord Id or Email Address</label>
+              <label htmlFor="email" className="text-sm font-medium">Enter Discord Id or Email Address</label>
 
               <div className="relative mt-1">
                 <input
-                  type="username"
-                  id="username"
+                  type="text"
+                  onChange={e => (email.current = e.target.value)}
                   className="w-full rounded-lg dark:bg-[#1e2126] text-black dark:text-white border-gray-200 p-4 pr-12 text-sm shadow-sm"
                   placeholder="yourname#0000"
                 />
-                <span class="absolute inset-y-0 right-4 inline-flex items-center">
-                  <i class="bi bi-discord"></i>
+                <span className="absolute inset-y-0 right-4 inline-flex items-center">
+                  <i className="bi bi-discord"></i>
                 </span>
               </div>
             </div>
 
             <div>
-              <label for="password" className="text-sm font-medium">Your Message</label>
+              <label htmlFor="message" className="text-sm font-medium">Your Message</label>
 
               <div className="relative mt-1">
                 <textarea
-                  type="message"
                   id="message"
                   rows="4"
+                  onChange={e => (message.current = e.target.value)}
                   className="w-full dark:bg-[#1e2126] rounded-lg text-black dark:text-white border-gray-200 p-4 mb-2 pr-12 text-sm shadow-sm"
                   placeholder="Drop your message. I will reply back soon."
                 />
@@ -135,15 +174,20 @@ const Home = ({ data, data2 }) => {
 
             <button
               type="submit"
+              onClick={sendMessage}
               className="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white"
             >
-              Send Message   <i class="bi bi-chat-left ml-2"></i>
+              <span>
+              Send Message
+                    {sending && <i className="bi bi-arrow-clockwise ml-2 animate-spin"></i>}
+                    {!sending && <i className="bi bi-chat-left ml-2"></i>}
+              </span>
             </button>
 
-            <p className="text-center hidden text-sm text-gray-500">
-              Error Message Here
+            <p className="text-center text-sm text-gray-500">
+              {errMsg}
             </p>
-          </form>
+          </htmlForm>
         </div>
 
       </div>
