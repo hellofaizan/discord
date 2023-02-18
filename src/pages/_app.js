7ea85a019bcaaa84b14503b8270c0645910e9c3c
Mohammad Faizan <84437051+hellofaizan@users.noreply.github.com>
2023-02-19 02:28:02 +0530

    Meta Data

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,29 +5,32 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 export default function App({ Component, pageProps }) {
   return (
     <>
-    <Head>
-        <meta charSet="utf-8" />
-        <title>HelloFaizan - Software Enthusiast</title>
-        {/* favicon */}
-        <link rel="icon" href="/faizan.png" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <meta name="theme-color" content="#171717" />
-        <meta
-          name="keywords"
-          content="HelloFaizan, Faizan Blog, HelloFaizan blog, CuriousFaizan, web developer, github, typescript, nextjs"
-        />
-        <meta name="description" content="HelloFaizan - Chech out all socials of Hellofaizan at one place" />
-        <meta name="author" content="Hello Faizan" />
-        <meta property="og:title" content="Own Linktree of HelloFaizan" />
-        <meta
-          property="og:description"
-          content="This website has all links to my social media accounts and more. Have a visit."
-        />
-        <meta
-          property="og:image"
-          content="/faizan.png"
-        />
-        <link rel="apple-touch-icon" href="/faizan.png" />
+      <Head>
+        {/* <!-- COMMON TAGS --> */}
+        <meta charset="utf-8" />/
+        <title>Hello Faizan - Software Enthusiast</title>
+        {/* <!-- Search Engine --> */}
+        <meta name="description" content="Chech out all socials of Hellofaizan at one place" />
+        <meta name="image" content="https://cdn.discordapp.com/attachments/1065518726855807067/1065518819377950790/cover-image_4.png" />
+        {/* <!-- Schema.org for Google --> */}
+        <meta itemprop="name" content="Hello Faizan - Software Enthusiast" />
+        <meta itemprop="description" content="Chech out all socials of Hellofaizan at one place" />
+        <meta itemprop="image" content="https://cdn.discordapp.com/attachments/1065518726855807067/1065518819377950790/cover-image_4.png" />
+        {/* <!-- Twitter --> */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Hello Faizan - Software Enthusiast" />
+        <meta name="twitter:description" content="Chech out all socials of Hellofaizan at one place" />
+        <meta name="twitter:site" content="@hellofaizandev" />
+        <meta name="twitter:image:src" content="https://cdn.discordapp.com/attachments/1065518726855807067/1065518819377950790/cover-image_4.png" />
+        {/* <!-- Open Graph general (Facebook, Pinterest & Google+) --> */}
+        <meta name="og:title" content="Hello Faizan - Software Enthusiast" />
+        <meta name="og:description" content="Chech out all socials of Hellofaizan at one place" />
+        <meta name="og:image" content="/faizan.png" />
+        <meta name="og:url" content="https://l.hellofaizan.me" />
+        <meta name="og:site_name" content="Hello Faizan Social Media Links" />
+        <meta name="og:locale" content="en_IN" />
+        <meta name="fb:admins" content="100008806001503" />
+        <meta name="og:type" content="website" />
       </Head>
       <div className="text-black dark:text-white flex flex-row w-full h-full bg-gradient-to-bl from-white to-[#e0e0e0] dark:from-[#171717] dark:to-[#1c1c1c] min-h-screen">
         <Component {...pageProps} />
